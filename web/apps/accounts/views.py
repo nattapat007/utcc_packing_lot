2f0f37c41da2e3e72b7a8a301ff1b063fe126c4d
nattapatCODIUM <nattapat.s@codium.co>
2020-10-23 19:58:34 +0700

    signup page

--- web/apps/accounts/views.py
+++ web/apps/accounts/views.py
@@ -1,4 +1,6 @@
 # Create your views here.
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -10,6 +12,26 @@ class SignupPageView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "signup.html"
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+
+        user = User.objects.create_user(username=username, password=password, email=email)
+
+        data = {
+            'user': user,
+            'created_user': user,
+            'updated_user': user,
+            'family_name': request.POST.get('family_name'),
+            'last_name': request.POST.get('last_name'),
+            'phone_number': request.POST.get('phone'),
+            'image': request.FILES.get('image')
+        }
+
+        UserProfile.objects.create(**data)
+        return redirect(self.success_url)
 
 
 class ProfilePageView(generic.UpdateView):
