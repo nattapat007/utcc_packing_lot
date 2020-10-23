0b067ea6c758d31879db87863f5b96f96d23bfb9
nattapatCODIUM <nattapat.s@codium.co>
2020-10-23 20:54:19 +0700

    remove accounts app

--- web/apps/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import SignupPageView, ProfilePageView
-
-urlpatterns = [
-
-    path("signup/", SignupPageView.as_view(), name="signup"),
-    path("<int:pk>/profile/", ProfilePageView.as_view(), name="profile"),
-]
