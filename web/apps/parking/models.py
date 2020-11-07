70a23358963fbb1218cdf99cd31fdc12ff973d1f
nattapatCODIUM <nattapat.s@codium.co>
2020-11-08 02:11:43 +0700

    BE: create status in park model and migrations file

--- web/apps/parking/models.py
+++ web/apps/parking/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 from model_controller.models import AbstractModelController
 
+from web.apps.commons.choices import InOutStatus
 from web.apps.motorcycle.models import Motorcycle
 from web.apps.user_profile.models import UserProfile
 
@@ -10,6 +11,7 @@ from web.apps.user_profile.models import UserProfile
 class Park(AbstractModelController):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, db_index=True)
     motorcycle = models.ForeignKey(Motorcycle, on_delete=models.CASCADE, db_index=True)
+    status = models.IntegerField(choices=InOutStatus.choices, default=InOutStatus.CHECKIN, db_index=True)
 
     class Meta:
         verbose_name = 'Park'
