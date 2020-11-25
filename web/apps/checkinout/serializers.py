f06ea4651f3e4a7d2229bac2afeef62f8c14c164
nattapatCODIUM <nattapat.s@codium.co>
2020-11-25 13:42:27 +0700

    checkinout using image base64

--- web/apps/checkinout/serializers.py
+++ web/apps/checkinout/serializers.py
@@ -2,8 +2,11 @@ from rest_framework import serializers
 from web.apps.parking.serializers import ParkSerializer
 from web.apps.checkinout.models import CheckIn, CheckOut
 
+from drf_extra_fields.fields import Base64ImageField
+
 
 class CheckInSerializer(serializers.ModelSerializer):
+    face_login = Base64ImageField()
     park = ParkSerializer(many=True, read_only=True, source='park_set')
 
     class Meta:
@@ -12,6 +15,7 @@ class CheckInSerializer(serializers.ModelSerializer):
 
 
 class CheckOutSerializer(serializers.ModelSerializer):
+    face_logout = Base64ImageField()
     park = ParkSerializer(many=True, read_only=True, source='park_set')
 
     class Meta:
