f5ea17434f67875da0038de8f1f4e62ec590f2b8
nattapatCODIUM <nattapat.s@codium.co>
2020-11-27 23:05:58 +0700

    add condition userprofile not exists

--- web/apps/user_profile/management/commands/init_data.py
+++ web/apps/user_profile/management/commands/init_data.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
+from web.apps.user_profile.models import UserProfile
+
 
 class Command(BaseCommand):
     help = 'Create initial project data'
@@ -12,9 +14,18 @@ class Command(BaseCommand):
 
     @staticmethod
     def _create_initial_user():
-        if not User.objects.exists():
-            User.objects.create_superuser(
+        if not User.objects.exists() and not UserProfile.objects.exists():
+            user = User.objects.create_superuser(
                 username='admin',
                 email='admin@utcc.ac.th',
                 password='ictutcc'
+            )
+
+            UserProfile.objects.create(
+                user=user,
+                family_name='ICT',
+                last_name='ICT',
+                phone_number=00000,
+                created_user=user,
+                updated_user=user
             )
