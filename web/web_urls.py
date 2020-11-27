9e2cc1aae33ce2ea2a4e0a820a9eb4ea7935dad4
nattapatCODIUM <nattapat.s@codium.co>
2020-11-28 00:18:05 +0700

    park export excel

--- web/web_urls.py
+++ web/web_urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 
+from web.apps.parking.export_park import export_park_xls
+
 urlpatterns = [
     path('', include('web.apps.pages.urls')),
     path('user_profile/', include('web.apps.user_profile.urls')),
+    path('export/park/', export_park_xls, name='export_park_xls'),
 ]
