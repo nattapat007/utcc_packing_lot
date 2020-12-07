ac5194ff80feb252302979b12d8460aa49371507
nattapatCODIUM <nattapat.s@codium.co>
2020-12-07 10:08:28 +0700

    add page urls

--- web/apps/pages/urls.py
+++ web/apps/pages/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+
 from .views import HomePageView, DetectPageView, CheckinPageView, CheckoutPageView
+from ..parking.views import ParkingExportView
+
+app_name = 'page'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('detect/', DetectPageView.as_view(), name='detect'),
     path('checkin/', CheckinPageView.as_view(), name='checkin'),
     path('checkout/', CheckoutPageView.as_view(), name='checkout'),
+    path('export/', ParkingExportView.as_view(), name='export'),
 ]
--- web/web_urls.py
+++ web/web_urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, include
 
-from web.apps.parking.export_park import export_park_xls
-
 urlpatterns = [
-    path('', include('web.apps.pages.urls')),
-    path('user_profile/', include('web.apps.user_profile.urls')),
-    path('export/park/', export_park_xls, name='export_park_xls'),
+    path('', include('web.apps.pages.urls', namespace='home')),
+    path('user_profile/', include('web.apps.user_profile.urls', namespace='user_profile')),
+    path('page/', include('web.apps.pages.urls', namespace='page')),
 ]
